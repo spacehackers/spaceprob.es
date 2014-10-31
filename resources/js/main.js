468cd60193a8e6c91a52c42beb04f568e78b6a9d
lisa ballard <lballard.cat@gmail.com>
2014-10-30 18:08:42 -0700

    changes class name of distance html element, starts using new json endpoint from api.spaceprobes to get distances of all spacecraft

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,13 +6,14 @@ $(document).ready(function() {
 
 var spaceprobes = {
 
-  dsn_data: {},
+  distances: {},
   probe_distances: {},
 
   init: function() {
 
     // fetch the dsn json feed
-    url = "http://murmuring-anchorage-8062.herokuapp.com/dsn/spaceprobes.json";
+    url = "http://murmuring-anchorage-8062.herokuapp.com/distances.json";
+    // url = "http://127.0.0.1:5000/distances.json";  // local
     $.ajax({
       url: url,
       type: 'get',
@@ -22,26 +23,24 @@ var spaceprobes = {
         $('#probes').slideDown("slow");
       },
       success: function(data) {
-        spaceprobes.dsn_data = data.spaceprobes;
+        spaceprobes.distances = data.spaceprobe_distances;
 
-        // update distance display for each probe
-        for (var dsn_name in spaceprobes.dsn_data) {
-          dsn_name = $.trim(dsn_name);
-          distance = Number(spaceprobes.dsn_data[dsn_name]['uplegRange']);
+        // first update distance display for each probe
+        for (var slug in spaceprobes.distances) {
+          distance = Number(spaceprobes.distances[slug]);
           if (distance > 0) {
               distance_str = numeral(distance).format('0.00 a');
-              elem = 'span[class=' + dsn_name + ']';
-              $(elem, '.distance').html(distance_str);
-              spaceprobes.probe_distances[dsn_name] = distance;
+              elem = 'span[class=' + slug + ']';
+              $(elem).html(distance_str);
+              spaceprobes.probe_distances[slug] = distance;
           }
         }
 
         // rearrange the homepage probes to be in distance order
-        if ($('#probes').is(":hidden")) {
-            // this is the homepage, rearrange the probes and display them
+        if ($('#probes').is(":hidden")) {  // true if this is homepage
 
             // sort the probes by distance
-            dsn_probes_sorted = Object.keys(spaceprobes.probe_distances).sort(function(a,b){return spaceprobes.probe_distances[b]-spaceprobes.probe_distances[a]; });
+            api_probes_sorted = Object.keys(spaceprobes.probe_distances).sort(function(a,b){return spaceprobes.probe_distances[b]-spaceprobes.probe_distances[a]; });
 
             // grab each probe snippet from the homepage #probes div and then empty the div
             probe_snippets = {};
@@ -50,10 +49,10 @@ var spaceprobes = {
             $('#probes').children().each(function() {
                 count = count + 1;
                 title = $(this).data('title');
-                dsn = $(this).data('dsn');
+                slug = $(this).data('slug');
                 my_list.push(title);
-                if (dsn.length) {
-                    probe_snippets[dsn] = $(this);
+                if (slug.length) {
+                    probe_snippets[slug] = $(this);
                 } else {
                     probe_snippets[title] = $(this);
                 }
@@ -62,8 +61,8 @@ var spaceprobes = {
 
 
             // re-append all the snippets in order of distance
-            for (var k in dsn_probes_sorted) {
-                title = dsn_probes_sorted[k];
+            for (var k in api_probes_sorted) {
+                title = api_probes_sorted[k];
                 if (title in probe_snippets) {
                     // append to #probes
                     $('#probes').append(probe_snippets[title]);
