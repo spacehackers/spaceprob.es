91323ac83645f1ea697ee9b6cf14ae7f2b5c890d
lisab <lisab@scribd.com>
2021-02-21 21:49:29 -0800

    remove deploy directory

--- deploy/README.md
+++
@@ -1,8 +0,0 @@
-This is a bit of python to deploy the static site to a development server.
-
-Run it like so:
-
-    fab deploy staging
-
-    fab deploy production
-
--- deploy/secrets_template.py
+++
@@ -1,6 +0,0 @@
-GIT_REPO = "file:////LOCAL/PATH/TO/spaceprobes/"  # local repo location
-LOCAL_TEMP_DIR = '/some/temporary/path/different/than/GIT_REPO/'  # some local directory to check out the repo, will be deleted!
-DEV_USER = 'DEV_SERVER_USER'
-DEV_URL = 'www.DEV_SERVER_URL.com'
-DEV_PATH = '~/DEV_SERVER_WEB_ROOT/spaceprobes'  # no trailing slash
-STAGING = {'DEV_USER':'', 'DEV_URL','DEV_PATH'}
